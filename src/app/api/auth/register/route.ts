a0fedc87d689884f7f16d0f0b9f34d30688de16e
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-08-26 14:28:41 +0500

    added api for login admin

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
-import connectDB from "../../../../libs/mongodb";
-import User from "../../../../models/User";
+import connectDB from "../../../../../libs/mongodb";
+import User from "../../../../../models/User";
 import bcrypt from "bcryptjs";
 
 export async function POST(request: Request) {
