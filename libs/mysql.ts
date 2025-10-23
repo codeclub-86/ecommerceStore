bda8af9a87b9aaff77604454665eff0a6cdd7393
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-10-23 18:30:18 +0500

    Bugs fixed

--- libs/mysql.ts
+++ libs/mysql.ts
@@ -5,7 +5,7 @@ let connection: any;
 export async function getConnection() {
   if (!connection) {
     connection = await mysql.createConnection({
-      host: "151.106.124.201",
+      host: "srv482.hstgr.io",
       user: "u224085852_info",
       password: "Codeclub@68",
       database: "u224085852_haasil_db",
