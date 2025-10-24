899d4016c0574d1e7be50b662f4b50ce4d177d10
Mazhar  ahmad <mazharahmad862@gmail.com>
2025-10-24 16:51:34 +0500

    Bugs fixed

--- libs/mysql.ts
+++ libs/mysql.ts
@@ -1,17 +1,15 @@
 import mysql from "mysql2/promise";
 
-let connection: any;
+const pool = mysql.createPool({
+  host: "151.106.124.201",
+  user: "u224085852_info",
+  password: "Codeclub@68",
+  database: "u224085852_haasil_db",
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
-export async function getConnection() {
-  if (!connection) {
-    connection = await mysql.createConnection({
-      host: "151.106.124.201",
-      user: "u224085852_info",
-      password: "Codeclub@68",
-      database: "u224085852_haasil_db",
-    });
-  }
-  return connection;
-
-
+export function getConnection() {
+  return pool;
 }
